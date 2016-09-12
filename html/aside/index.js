fe0affbb8e43ec0127007b66af570831c4d93107
qiongtubao <810776886@qq.com>
2016-09-12 15:09:12 +0800

    add aside module

--- html/aside/index.js
+++ html/aside/index.js
@@ -10,16 +10,18 @@ var latte_lib = latte.require("latte_lib");
   var data = latte_lib.object.create({
     aside: {
     	placement: "left",
+    	title: "title",
     	data: {
     		
     	}
 	},
 	show: function() {
-		this.set("alert.show", 1);
-		
+		this.get("aside").show();
+		/**
 		setTimeout(function() {
 			data.set("alert.show", 0)
 ;		}, 2000);
+		*/
 	}
   });
   var box = latte_dom.define("demo", data);
--- html/latte_dom_strap.js
+++ html/latte_dom_strap.js
@@ -283,9 +283,106 @@ function(require, exports, module, window) {
 define("latte_dom/c/commands/aside.js", ["require", "exports", "module", "window"],
 function(require, exports, module, window) {
 (function() {
-	//导航栏
+	//aside
+	var backdrop;
+	var getBackdrop = function() {
+		if(backdrop) {
+			return backdrop;
+		}
+		var backdropDom = document.createElement("div");
+		var View = require("../../v/view.js");
+		backdrop = View.create(backdropDom);
+		backdrop.classed({
+			"aside-backdrop": 1,
+			"ease-fade": 1
+		});
+		document.body.appendChild(backdropDom);
+		backdrop.show = function() {
+			if(!backdrop) {
+				createBackDrop();
+			}
+			backdrop.style("display", "block");
+			//这里需要让它之行css3的动画
+			backdrop.classed("in", 1);
+			return ++self.zIndex;
+		}
+		backdrop.hide = function() {
+			backdrop.style("display", "none");
+			backdrop.classed("in", 0);
+		}
+		backdrop.zIndex = 1040;
+		return backdrop;
+	}
+	var cd = function(name) {
+		return document.createElement(name || "div");
+	}
 	this.before = function(data, dom, controller) {
-		
+		var aside = dom.attr("latte-strap-aside");
+		var placement = dom.attr("latte-aside-placement") || "left";
+		if(aside) {		
+						var bodyDom = cd();
+						bodyDom.setAttribute("latte-data", "data");
+						bodyDom.className += " aside-body";
+								var spanDom = cd("span");
+								spanDom.innerHTML  = "x";
+							var closeDom = cd("button");
+							closeDom.className = "close";
+							closeDom.setAttribute("latte-click", "close");
+							closeDom.appendChild(spanDom);
+
+							var titleDom = cd("h4");
+							titleDom.className = "aside-title";
+							titleDom.setAttribute("latte-html", "{{title}}");
+						var headerDom = cd();
+						headerDom.className = "aside-header";
+						headerDom.appendChild(closeDom);
+						headerDom.appendChild(titleDom);
+					var contentDom = cd();
+					contentDom.appendChild(headerDom);
+
+
+					contentDom.appendChild(bodyDom);
+					contentDom.className = "aside-content";
+
+				var dialogDom = cd();
+				dialogDom.className = "aside-dialog";
+				dialogDom.appendChild(contentDom);
+			var asideDom = cd();
+			//先固定吧 之后在看情况是不是要变成动态的
+			asideDom.className = "aside "+"slide"+placement+"-transition "+placement ;;
+			asideDom.setAttribute("latte-show", "show");
+			asideDom.appendChild(dialogDom);
+			var b ;
+			while(b = dom.children[0]) {
+				dom.removeChild(b);	
+				bodyDom.appendChild(b);
+			}
+			var change = function(value, old) {
+				if(!value) {return;}
+				var Controller = require("../controller.js");
+				if(old) {
+					Controller.removeChild(dom, old);
+				}
+				dom.html("");
+				var copyDom = asideDom.cloneNode(true);
+				dom.appendChild(copyDom);
+				value.on("show", function(value, old) {
+					if(value == old) { return; }
+					value ? getBackdrop().show(): getBackdrop().hide();
+				});
+				value.set("close", function() {
+					value.set("show", false);
+				});
+				value.show = function() {
+					value.set("show", true);
+					getBackdrop().show();
+				};
+				Controller.createChild(dom, value);
+			}
+			console.log(data, aside);
+			change(data.get(aside));
+			controller.bind("data",  aside, change);
+		}
 	}
 }).call(module.exports);
 });
@@ -431,10 +528,13 @@ function(require, exports, module, window) {
 			div.setAttribute("latte-class", "{{size}}")
 			div.appendChild(spinner);
 			dom.appendChild(div);
-				var change = function() {
+				var change = function(value, old) {
+					if(old) {
+						Controller.removeChild(dom, old);
+					}
 					dom.html("");
 					dom.appendChild(div);
-					Controller.createChild(dom);
+					Controller.createChild(dom, value);
 				}
 			controller.bind("data", spinner, change);
 		}
@@ -469,7 +569,7 @@ function(require, exports, module, window) {
 define("latte_dom/c/commands/typeahead.js", ["require", "exports", "module", "window"],
 function(require, exports, module, window) {
 (function() {
-	//导航栏
+	//
 	this.before = function(data, dom, controller) {
 		
 	}
